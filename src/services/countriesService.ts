aac7b7cc071562fe68f41a344a36467a96426c6c
João Miguel <joaomiguel.apr@gmail.com>
2025-11-05 02:09:38 -0300

    feat: creation of function getNamesAndFlags

--- src/services/countriesService.ts
+++ src/services/countriesService.ts
@@ -1,13 +1,13 @@
 async function getAllCountries(){
-    let response = await fetch('https://restcountries.com/v3.1/all?fields=https://restcountries.com/v3.1/all?fields=translations,flags,flags')
+    let response = await fetch('https://restcountries.com/v3.1/all?fields=translations,flags')
     let countries = await response.json()
     return countries
 }
 
-export async function getSelectedCountries(){
+export async function getNamesAndFlags(){
     let countries = await getAllCountries()
     countries = countries.map(country => ({
-        name: country.translations?.por?.common || country?.name?.common || 'No name',
+        name: country.translations?.por?.common,
         flag: country.flags.png
     }))
     return countries
