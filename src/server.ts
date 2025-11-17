f99e48013c6a386b4b2cc3acb015a1307bb2e969
João Miguel <joaomiguel.apr@gmail.com>
2025-11-16 23:17:38 -0300

    feat: add CORS

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import fastify from "fastify"
+import cors from "@fastify/cors"
 import { registerRoutes } from "./router.js"
 
 export const app = fastify({logger: true})
 
+await app.register(cors)
 await registerRoutes(app)
 
 try {
-    await app.listen({port:3000})
+    await app.listen({port:3333})
 } catch (err){
     app.log.error(err)
 }
